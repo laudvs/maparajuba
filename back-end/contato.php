411513169595c0fe61e0ffe2dfbbc0fa93148c2d
Laurah <115381945+laudvs@users.noreply.github.com>
2025-09-16 20:02:10 -0300

    Update contato.php

--- back-end/contato.php
+++ back-end/contato.php
@@ -1,31 +1,38 @@
 <?php
-// Configuração do banco
-$servername = "localhost";
-$username = "root";  // padrão do XAMPP
-$password = "";      // senha padrão é vazia
-$dbname = "advocacia";
+// Configuração do banco de dados
+$host = "localhost";
+$usuario = "root";   // ajuste conforme seu ambiente
+$senha = "";         // ajuste conforme seu ambiente
+$banco = "maparajuba_db";
 
 // Criar conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $usuario, $senha, $banco);
 
 // Verificar conexão
 if ($conn->connect_error) {
-    die("Erro de conexão: " . $conn->connect_error);
+    die("Falha na conexão: " . $conn->connect_error);
 }
 
-// Capturar dados do formulário
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$mensagem = $_POST['mensagem'];
+// Receber dados do formulário
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $conn->real_escape_string($_POST['nome']);
+    $email = $conn->real_escape_string($_POST['email']);
+    $mensagem = $conn->real_escape_string($_POST['mensagem']);
 
-// Inserir no banco
-$sql = "INSERT INTO contatos (nome, email, mensagem) VALUES ('$nome', '$email', '$mensagem')";
+    // Inserir no banco
+    $sql = "INSERT INTO contatos (nome, email, mensagem) 
+            VALUES ('$nome', '$email', '$mensagem')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "<p>Mensagem enviada com sucesso!</p>";
-    echo "<a href='../front-end/index.html'>Voltar</a>";
-} else {
-    echo "Erro: " . $sql . "<br>" . $conn->error;
+    if ($conn->query($sql) === TRUE) {
+        echo "<p style='color:green; font-family:Arial; text-align:center; margin-top:50px;'>
+                Obrigado, $nome! Sua mensagem foi enviada com sucesso.
+              </p>";
+        echo "<p style='text-align:center;'><a href='../front-end/index.html'>Voltar ao site</a></p>";
+    } else {
+        echo "<p style='color:red; font-family:Arial; text-align:center; margin-top:50px;'>
+                Erro: " . $conn->error . "
+              </p>";
+    }
 }
 
 $conn->close();
